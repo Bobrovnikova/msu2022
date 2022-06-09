7b667135ff87b8801d018451e86b463e4501b063
Bobrovnikova <101701003+Bobrovnikova@users.noreply.github.com>
2022-06-10 01:23:28 +0300

    colors b

--- main.c
+++ main.c
@@ -55,8 +55,8 @@ int main() {
 
     newImage = color_to_gray(idata, iw, ih, n);
     int t_black = 64;
-    int t_white = 202;
-    int t_gray = 100;
+    int t_white = 207;
+    int t_gray = 95;
     gray_to_bw(newImage, iw, ih, t_black, t_white, t_gray);
    
     char* outputPath = "result.png";
