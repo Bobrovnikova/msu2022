5282aa751682ef66d18dbdf3f83e69810789a441
Bobrovnikova <101701003+Bobrovnikova@users.noreply.github.com>
2022-06-10 19:18:03 +0300

    dfs

--- main.c
+++ main.c
@@ -16,28 +16,28 @@ int forsort(const void* x, const void* y) {
 void dfs(int i, int j int color, int sizeV, int sizeH, int* mark, unsigned char * Image){
     mark[i*sizeV+j] = color;
     if(abs(Image[i*sizeV+j]-Image[sizeV*(i+1)+j])<E&&!mark[sizeV*(i+1)+j]){
-        dfs(sizeV*(i+1)+j, color, sizeV, sizeH, mark, Image);
+        dfs(i+1, j, color, sizeV, sizeH, mark, Image);
     }
     if(abs(Image[i*sizeV+j]-Image[sizeV*i+j+1])<E&&!mark[sizeV*i+j+1]){
-        dfs(sizeV*i+j+1, color, sizeV, sizeH, mark, Image);
+        dfs(i,j+1, color, sizeV, sizeH, mark, Image);
     }
     if(abs(Image[i*sizeV+j]-Image[sizeV*(i+1)+j+1])<E&&!mark[sizeV*(i+1)+j+1]){
-        dfs(sizeV*(i+1)+j+1, color, sizeV, sizeH, mark, Image);
+        dfs(i+1,j+1, color, sizeV, sizeH, mark, Image);
     }
     if(abs(Image[i*sizeV+j]-Image[sizeV*(i-1)+j])<E&&!mark[sizeV*(i-1)+j]){
-        dfs(sizeV*(i-1)+j, color, sizeV, sizeH, mark, Image);
+        dfs(i-1,j, color, sizeV, sizeH, mark, Image);
     }
     if(abs(Image[i*sizeV+j]-Image[sizeV*i+j-1])<E&&!mark[sizeV*i+j-1]){
-        dfs(sizeV*i+j-1, color, sizeV, sizeH, mark, Image);
+        dfs(i,j-1, color, sizeV, sizeH, mark, Image);
     }
     if(abs(Image[i*sizeV+j]-Image[sizeV*(i-1)+j-1])<E&&!mark[sizeV*(i-1)+j-1]){
-        dfs(sizeV*(i-1)+j-1, color, sizeV, sizeH, mark, Image);
+        dfs(i-1,j-1, color, sizeV, sizeH, mark, Image);
     }
     if(abs(Image[i*sizeV+j]-Image[sizeV*(i-1)+j+1])<E&&!mark[sizeV*(i-1)+j+1]){
-        dfs(sizeV*(i-1)+j+1, color, sizeV, sizeH, mark, Image);
+        dfs(i-1,j+1, color, sizeV, sizeH, mark, Image);
     }
     if(abs(Image[i*sizeV+j]-Image[sizeV*(i+1)+j-1])<E&&!mark[sizeV*(i+1)+j-1]){
-        dfs(sizeV*(i+1)+j-1, color, sizeV, sizeH, mark, Image);
+        dfs(i+1,j-1, color, sizeV, sizeH, mark, Image);
     }
     return;
 }
