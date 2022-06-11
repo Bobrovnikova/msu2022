ea5ce0dffe1e5966742db520351cf43539dd2e37
Bobrovnikova <101701003+Bobrovnikova@users.noreply.github.com>
2022-06-11 23:26:27 +0300

    dfs separate if

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ void dfs(int i, int j, int color, int sizeV, int sizeH, unsigned char * mark, un
         dfs(i,j+1, color, sizeV, sizeH, mark, Image);
     }
     if(((i+1)<(sizeH-1))&&((j+1)<(sizeV-1)))
-    if(iabs(Image[i*sizeV+j]-Image[sizeV*(i+1)+j+1])<E&&!mark[sizeV*(i+1)+j+1]){
+    if(abs(Image[i*sizeV+j]-Image[sizeV*(i+1)+j+1])<E&&!mark[sizeV*(i+1)+j+1]){
         dfs(i+1,j+1, color, sizeV, sizeH, mark, Image);
     }
     /*if(i-1>2&&abs(Image[i*sizeV+j]-Image[sizeV*(i-1)+j])<E&&!mark[sizeV*(i-1)+j]){
