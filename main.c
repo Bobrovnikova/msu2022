bca73b24eaf636e426c09ffb75dcf50e66f09caa
Bobrovnikova <101701003+Bobrovnikova@users.noreply.github.com>
2022-06-11 22:31:33 +0300

    bw+dfs

--- main.c
+++ main.c
@@ -146,7 +146,7 @@ int main() {
     int t_black = 97;
     int t_white = 220;
     int t_gray = 150;
-    //gray_to_bw(newImage, iw, ih, t_black, t_white, t_gray);
+    gray_to_bw(newImage, iw, ih, t_black, t_white, t_gray);
     //gaus_filter(newImage, iw, ih);
     //gaus_filter(newImage, iw, ih);
     //unsigned char* colorImage = (unsigned char*)malloc(iw*ih*n*sizeof(unsigned char));
